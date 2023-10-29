ca5cb6faac5e98ceb97096eb7e7ee39c3749af36
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2023-10-30 01:59:20 +0200

    wip

--- scripts/build.ts
+++ scripts/build.ts
@@ -6,6 +6,7 @@ await rimraf('build')
 
 await $`mkdir dist`;
 
+// CJS
 await $`wasm-pack build --target nodejs --weak-refs --release --out-dir build --out-name index`;
 
 await $`cp build/index_bg.wasm dist/index_bg.wasm`;
@@ -15,9 +16,16 @@ await $`cp build/index.d.ts dist/index.d.ts`;
 
 await rimraf('build')
 
+// ESM + WASM (for bundlers)
 await $`wasm-pack build --target bundler --weak-refs --release --out-dir build --out-name index`;
 
 await $`cp build/index.js dist/index.js`;
 await $`cp build/index_bg.js dist/index_bg.js`;
+
+// ESM + WASM (for browsers)
+await $`wasm-pack build --target web --weak-refs --release --out-dir build --out-name index`;
+
+await $`cp build/index.js dist/init.js`;
+await $`cp build/index.d.ts dist/init.d.ts`;
 
 await rimraf('build')
